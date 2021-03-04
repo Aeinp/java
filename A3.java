377bff4c3660565b587a07420169cdc9bdff6652
Aeinp <51356525+Aeinp@users.noreply.github.com>
2021-03-04 08:58:04 +0530

    Update A3.java

--- A3.java
+++ A3.java
@@ -1,37 +1,27 @@
-package exam;
+import java.util.Scanner;
 
-class A3 
-{ 
-    static final int n = 5; 
-    static void printSumSimple(int mat[][], int k) 
-    { 
-  
-        if (k > n) return; 
-          
-         
-        for (int i = 0; i < n-k+1; i++) 
-        { 
-          for (int j = 0; j < n-k+1; j++) 
-            { 
-                int sum = 0; 
-                for (int p = i; p < k+i; p++) 
-                    for (int q = j; q < k+j; q++) 
-                        sum += mat[p][q]; 
-  
-                System.out.print(sum+ " "); 
-            } 
-            System.out.println(); 
-        } 
-    } 
-      
-    public static void main(String arg[]) 
-    { 
-        int mat[][] = {{1, 1, 1, 1, 1}, 
-                       {2, 2, 2, 2, 2}, 
-                       {3, 3, 3, 3, 3}, 
-                       {4, 4, 4, 4, 4}, 
-                       {5, 5, 5, 5, 5}}; 
-        int k = 3; 
-        printSumSimple(mat, k); 
-    } 
-} 
+public class SqareMatrix 
+{
+	public static void main(String[] args) 
+	{
+		Scanner in=new Scanner(System.in);
+		while(true)
+		{
+			System.out.print("Enter the value of n for n-Square Matrix : ");
+			int n=in.nextInt();
+			if(n<=0)
+				System.out.println("invalid input");
+			else
+			{
+				int sum=0;
+				while(n!=0)
+				{
+					sum=sum+n*n;
+					n--;
+				}
+				System.out.println("The number square matrices are : " +sum);
+			}
+		}
+	}
+}
+
